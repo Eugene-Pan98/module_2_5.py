1ad258beb386547c4a4e017eb098d7a394fe1ce0
Eugene-Pan98 <vb02@list.ru>
2024-08-15 17:09:05 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -2,8 +2,8 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         line = []
-    for j in range(m):
-        line.append(value)
+        for j in range(m):
+            line.append(value)
         matrix.append(line)
     return matrix
 
